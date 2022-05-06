f5b1da7fb168951b5e99da2281e6f544e6faec3b
romaricl2l3m <romaric.tiangaye@gmail.com>
2022-05-06 16:16:47 +0200

    correction

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		// final int index;
 		for(int i=0; i<=6; i++) {
-			System.out.println("Hello bugs !");
+			System.out.println("hello bugs !");
 		}
 	}
 }
